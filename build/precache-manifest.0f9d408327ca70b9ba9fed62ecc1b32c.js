6f3bd02f41f1656c2dbd031f3491834e170d398b
Darina <kolodad00@gmail.com>
2020-08-16 15:14:32 +0300

    gh-pages add

---
+++ build/precache-manifest.0f9d408327ca70b9ba9fed62ecc1b32c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072b726909414d42e595fd837969898a",
+    "url": "/Calculator-Money/index.html"
+  },
+  {
+    "revision": "6851c48b1f07f930202a",
+    "url": "/Calculator-Money/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "194c9a65ab1d28e26a88",
+    "url": "/Calculator-Money/static/js/2.3da6aa10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-Money/static/js/2.3da6aa10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6851c48b1f07f930202a",
+    "url": "/Calculator-Money/static/js/main.b96001c3.chunk.js"
+  },
+  {
+    "revision": "740f35e90ea4f1eb3a55",
+    "url": "/Calculator-Money/static/js/runtime-main.2f2ded10.js"
+  }
+]);
